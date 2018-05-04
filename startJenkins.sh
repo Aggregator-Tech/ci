f3beb5df352af4909b675402a5268523dcb6816d
root <root@den01yvn.us.oracle.com>
2018-05-04 11:52:16 -0700

    changes

--- startJenkins.sh
+++ startJenkins.sh
@@ -1,16 +1,5 @@
-#do we need to check if jenkins-dv exists
-#if it does, do we need to delete it and create new 
 echo "Stopping running container aggregatortech-jenkins"
 docker stop aggregatortech-jenkins
-docker container rm -f aggregatortech-jenkins
-echo "Removing aggregatortech-jenkins-dv,aggregatortech-jenkins"
-docker container rm -f aggregatortech-jenkins
-docker container rm -f aggregatortech-jenkins-dv
-docker volume rm -f aggregatortech-jenkins-dv
-echo "volume removed successfully"
-docker create -v /var/jenkins_home --name aggregatortech-jenkins-dv jenkins/jenkins
-echo "volume recreated  successfully"
-docker cp  aggregatortech-jenkins-dv-repo/jenkins_home  aggregatortech-jenkins-dv:/var
-echo "volume refreshed  successfully"
+docker rm  aggregatortech-jenkins
 docker run -p 8080:8080 -p 50000:50000 --volumes-from aggregatortech-jenkins-dv --env JAVA_OPTS="-Dhttps.proxyHost=www-proxy.us.oracle.com -Dhttps.proxyPort=80 -Dhttp.proxyHost=www-proxy.us.oracle.com -Dhttp.proxyPort=80" --name aggregatortech-jenkins jenkins/jenkins
 
