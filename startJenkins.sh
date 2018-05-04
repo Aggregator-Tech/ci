6e0bb62b8fe3a4be3ee93948c5a1f7e167ede55b
root <root@den01yvn.us.oracle.com>
2018-05-03 23:50:02 -0700

    changed to add copy command

--- startJenkins.sh
+++ startJenkins.sh
@@ -1,6 +1,10 @@
 #do we need to check if jenkins-dv exists
 #if it does, do we need to delete it and create new 
-docker volume rm jenkins-dv
+docker volume rm -f jenkins-dv
+echo "volume removed successfully"
 docker create -v /var/jenkins_home --name jenkins-dv jenkins 
-docker run -p 8080:8080 -p 50000:50000 --volumes-from jenkins2-dv --env JAVA_OPTS="-Dhttps.proxyHost=www-proxy.us.oracle.com -Dhttps.proxyPort=80 -Dhttp.proxyHost=www-proxy.us.oracle.com -Dhttp.proxyPort=80" jenkins
+echo "volume recreated  successfully"
+docker cp  jenkins-dv/jenkins_home jenkins-dv:/var
+echo "volume refreshed  successfully"
+docker run -p 8080:8080 -p 50000:50000 --volumes-from jenkins-dv --env JAVA_OPTS="-Dhttps.proxyHost=www-proxy.us.oracle.com -Dhttps.proxyPort=80 -Dhttp.proxyHost=www-proxy.us.oracle.com -Dhttp.proxyPort=80" jenkins
 
