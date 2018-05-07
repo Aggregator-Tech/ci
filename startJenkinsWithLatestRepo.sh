688b8a967811a0bda31084550cffaf4ca67b8898
root <root@den01yvn.us.oracle.com>
2018-05-07 12:17:19 -0700

    change permission before copying the content to the data volume

--- startJenkinsWithLatestRepo.sh
+++ startJenkinsWithLatestRepo.sh
@@ -9,6 +9,7 @@ docker container rm -f aggregatortech-jenkins-dv
 echo "volume removed successfully"
 docker create -v /var/jenkins_home --name aggregatortech-jenkins-dv jenkins/jenkins
 echo "volume recreated  successfully"
+chmod -R 777  aggregatortech-jenkins-dv-repo
 docker cp  aggregatortech-jenkins-dv-repo/jenkins_home/.  aggregatortech-jenkins-dv:/var/jenkins_home/.
 echo "volume refreshed  successfully"
 docker run -p 8080:8080 -p 50000:50000 --volumes-from aggregatortech-jenkins-dv --env JAVA_OPTS="-Dhttps.proxyHost=www-proxy.us.oracle.com -Dhttps.proxyPort=80 -Dhttp.proxyHost=www-proxy.us.oracle.com -Dhttp.proxyPort=80" --name aggregatortech-jenkins jenkins/jenkins
