76ec7448c6d008590209d64c83d6663a78e2086d
root <root@den01yvn.us.oracle.com>
2018-05-08 16:48:11 -0700

    handling proxy

--- startJenkins.sh
+++ startJenkins.sh
@@ -11,10 +11,14 @@ if [ -z "$proxyPort" ]; then
    echo "No proxy port from cmd, use default"
    proxyPort="80"
 fi
+proxyString = "-Dhttps.proxyHost=$proxyHost -Dhttps.proxyPort=$proxyPort -Dhttp.proxyHost=$proxyHost -Dhttp.proxyPort=$proxyPort"
+if [ $proxyHost == "--noProxy" ] ; then
+	proxyString=""
+fi
 echo "Proxy host is"+$proxyHost
 echo "Proxy Port is"+$proxyPort
 echo "Stopping running container aggregatortech-jenkins"
 docker stop aggregatortech-jenkins
 docker rm  aggregatortech-jenkins
-docker run -p 8080:8080 -p 50000:50000 --volumes-from aggregatortech-jenkins-dv --env JAVA_OPTS="-Dhttps.proxyHost=$proxyHost -Dhttps.proxyPort=$proxyPort -Dhttp.proxyHost=$proxyHost -Dhttp.proxyPort=$proxyPort" --name aggregatortech-jenkins jenkins/jenkins
+docker run -p 8080:8080 -p 50000:50000 --volumes-from aggregatortech-jenkins-dv --env JAVA_OPTS="$proxyString" --name aggregatortech-jenkins jenkins/jenkins
 
--- startJenkinsWithLatestRepo.sh
+++ startJenkinsWithLatestRepo.sh
@@ -1,5 +1,7 @@
 #do we need to check if jenkins-dv exists
 #if it does, do we need to delete it and create new 
+mkdir stagingForJenkins
+mkdir stagingForJenkins/jenkins_home
 proxyHost=$1
 proxyPort=$2
 if [ -z "$proxyHost" ]; then
@@ -13,6 +15,12 @@ if [ -z "$proxyPort" ]; then
 fi
 echo "Proxy host is"+$proxyHost
 echo "Proxy Port is"+$proxyPort
+cp -r aggregatortech-jenkins-dv-repo/jenkins_home/.  stagingForJenkins/jenkins_home/.
+rm -f stagingForJenkins/jenkins_home/proxy.xml
+if [ $proxyHost != "--noProxy" ] ; then
+sed "s/\${http.proxyHost}/$proxyHost/;s/\${http.proxyPort}/$proxyPort/" aggregatortech-jenkins-dv-repo/jenkins_home/proxy.xml >>  stagingForJenkins/jenkins_home/proxy.xml
+fi
+docker cp  stagingForJenkins/jenkins_home/.  aggregatortech-jenkins-dv:/var/jenkins_home/.
 echo "Stopping running container aggregatortech-jenkins"
 docker stop aggregatortech-jenkins
 docker container rm -f aggregatortech-jenkins
@@ -22,13 +30,10 @@ docker container rm -f aggregatortech-jenkins-dv
 echo "volume removed successfully"
 docker create -v /var/jenkins_home --name aggregatortech-jenkins-dv jenkins/jenkins
 echo "volume recreated  successfully"
-rm proxy.xml
-sed "s/\${http.proxyHost}/$proxyHost/;s/\${http.proxyPort}/$proxyPort/" aggregatortech-jenkins-dv-repo/jenkins_home/proxy.xml >> proxy.xml
 
-chmod -R 777  aggregatortech-jenkins-dv-repo
-docker cp  aggregatortech-jenkins-dv-repo/jenkins_home/.  aggregatortech-jenkins-dv:/var/jenkins_home/.
-docker cp  proxy.xml  aggregatortech-jenkins-dv:/var/jenkins_home/.
-rm -rf proxy.xml
+chmod -R 777 stagingForJenkins
+docker cp  stagingForJenkins/jenkins_home/.  aggregatortech-jenkins-dv:/var/jenkins_home/.
 echo "volume refreshed  successfully"
+rm -rf stagingForJenkins
 sh startJenkins.sh $proxyHost $proxyPort
 
