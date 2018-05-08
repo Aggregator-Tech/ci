1bf332a90e6d694053f2cc9406ed12183970dc0a
root <root@den01yvn.us.oracle.com>
2018-05-08 14:50:27 -0700

    changed proxy

--- startJenkinsWithLatestRepo.sh
+++ startJenkinsWithLatestRepo.sh
@@ -1,5 +1,18 @@
 #do we need to check if jenkins-dv exists
 #if it does, do we need to delete it and create new 
+proxyHost=$1
+proxyPort=$2
+if [ -z "$proxyHost" ]; then
+   echo "No proxy Host from cmd"
+   proxyHost="www-proxy.us.oracle.com"
+  
+fi
+if [ -z "$proxyPort" ]; then
+   echo "No proxy port from cmd, use default"
+   proxyPort="80"
+fi
+echo "Proxy host is"+$proxyHost
+echo "Proxy Port is"+$proxyPort
 echo "Stopping running container aggregatortech-jenkins"
 docker stop aggregatortech-jenkins
 docker container rm -f aggregatortech-jenkins
@@ -9,8 +22,12 @@ docker container rm -f aggregatortech-jenkins-dv
 echo "volume removed successfully"
 docker create -v /var/jenkins_home --name aggregatortech-jenkins-dv jenkins/jenkins
 echo "volume recreated  successfully"
+rm proxy.xml
+sed "s/\${http.proxyHost}/$proxyHost/;s/\${http.proxyPort}/$proxyPort/" aggregatortech-jenkins-dv-repo/jenkins_home/proxy.xml >> proxy.xml
+
 chmod -R 777  aggregatortech-jenkins-dv-repo
 docker cp  aggregatortech-jenkins-dv-repo/jenkins_home/.  aggregatortech-jenkins-dv:/var/jenkins_home/.
+docker cp  proxy.xml  aggregatortech-jenkins-dv:/var/jenkins_home/.
 echo "volume refreshed  successfully"
-docker run -p 8080:8080 -p 50000:50000 --volumes-from aggregatortech-jenkins-dv --env JAVA_OPTS="-Dhttps.proxyHost=www-proxy.us.oracle.com -Dhttps.proxyPort=80 -Dhttp.proxyHost=www-proxy.us.oracle.com -Dhttp.proxyPort=80" --name aggregatortech-jenkins jenkins/jenkins
+sh startJenkins.sh $proxyHost $proxyPort
 
