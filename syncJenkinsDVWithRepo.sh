9a7d5fe62c9bb33101d8b6a2db1498c34d740366
root <root@den01yvn.us.oracle.com>
2018-05-08 12:09:16 -0700

    delete war

--- syncJenkinsDVWithRepo.sh
+++ syncJenkinsDVWithRepo.sh
@@ -1,5 +1,9 @@
 echo "Copying volume data from Jenikins Dv to local repository dir. "
 docker cp   aggregatortech-jenkins-dv:/var/jenkins_home/. aggregatortech-jenkins-dv-repo/jenkins_home/. 
 rm -rf aggregatortech-jenkins-dv-repo/jenkins_home/logs
+rm -rf aggregatortech-jenkins-dv-repo/jenkins_home/tools
+rm -rf aggregatortech-jenkins-dv-repo/jenkins_home/.gradle
+rm -rf aggregatortech-jenkins-dv-repo/jenkins_home/war
+
 echo "Local repo directory refreshed  successfully"
 
