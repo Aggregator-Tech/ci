b84c5aababbc07a801171741d14d229b522dd5c4
root <root@den01yvn.us.oracle.com>
2018-05-07 22:08:45 -0700

    removing logs dir

--- syncJenkinsDVWithRepo.sh
+++ syncJenkinsDVWithRepo.sh
@@ -1,4 +1,5 @@
 echo "Copying volume data from Jenikins Dv to local repository dir. "
 docker cp   aggregatortech-jenkins-dv:/var/jenkins_home/. aggregatortech-jenkins-dv-repo/jenkins_home/. 
+rm -rf aggregatortech-jenkins-dv-repo/jenkins_home/logs
 echo "Local repo directory refreshed  successfully"
 
