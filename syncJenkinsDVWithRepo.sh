14ae4c6c411a5bde30ae028738b7d04492f69578
atul-aggregatortech <atul.aggregatortech@gmail.com>
2018-05-17 19:58:54 -0700

    clean up

--- syncJenkinsDVWithRepo.sh
+++ syncJenkinsDVWithRepo.sh
@@ -5,6 +5,14 @@ mkdir stagingToRepo/jenkins_home
 docker cp   aggregatortech-jenkins-dv:/var/jenkins_home/. stagingToRepo/jenkins_home/. 
 #Items we dont want to copy over
 rm -rf stagingToRepo/jenkins_home/proxy.xml
+rm -rf stagingToRepo/jenkins_home/.bash_history
+rm -rf stagingToRepo/jenkins_home/.bash_logout
+rm -rf stagingToRepo/jenkins_home/.bashrc
+rm -rf stagingToRepo/jenkins_home/.gitconfig
+rm -rf stagingToRepo/jenkins_home/.java
+rm -rf stagingToRepo/jenkins_home/.owner
+rm -rf stagingToRepo/jenkins_home/.profile
+rm -rf stagingToRepo/jenkins_home/.oracle_jre_usage
 rm -rf stagingToRepo/jenkins_home/copy_reference_file.log
 rm -rf stagingToRepo/jenkins_home/queue.xml.bak
 rm -rf stagingToRepo/jenkins_home/logs
