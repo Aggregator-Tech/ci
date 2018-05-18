8c5589d17fc42fc674da375a981670c58fed7b43
root <root@den01yvn.us.oracle.com>
2018-05-18 14:59:54 -0700

    configure proxy and platform config at ne place

--- startJenkinsWithLatestRepo.sh
+++ startJenkinsWithLatestRepo.sh
@@ -4,7 +4,7 @@ mkdir stagingForJenkins
 mkdir stagingForJenkins/jenkins_home
 proxyHost=$1
 proxyPort=$2
-platform_url=$3
+platformurl=$3
 if [ -z "$proxyHost" ]; then
    echo "No proxy Host from cmd"
    proxyHost="www-proxy.us.oracle.com"
@@ -14,24 +14,23 @@ if [ -z "$proxyPort" ]; then
    echo "No proxy port from cmd, use default"
    proxyPort="80"
 fi
-if [ -z "$platform_url" ]; then
+if [ -z "$platformurl" ]; then
    echo "No platform url from cmd"
-   platform_url="https://aggregatortech-platform.herokuapp.com"
+   platformurl="aggregatortech-platform.herokuapp.com"
   
 fi
 
 echo "Proxy host is"+$proxyHost
 echo "Proxy Port is"+$proxyPort
+echo "Platfrom url is"+$platformurl
 cp -r aggregatortech-jenkins-dv-repo/jenkins_home/.  stagingForJenkins/jenkins_home/.
 rm -f stagingForJenkins/jenkins_home/proxy.xml
 rm -f stagingForJenkins/jenkins_home/config.xml
+
 if [ $proxyHost != "--noProxy" ] ; then
    sed "s/\${http.proxyHost}/$proxyHost/;s/\${http.proxyPort}/$proxyPort/" aggregatortech-jenkins-dv-repo/jenkins_home/proxy.xml >>  stagingForJenkins/jenkins_home/proxy.xml
-   sed "s/\${http.proxyHost}/$proxyHost/;s/\${http.proxyPort}/$proxyPort/" aggregatortech-jenkins-dv-repo/jenkins_home/config.xml >>  stagingForJenkins/jenkins_home/config.xml
 fi
-if [ -z "$platform_url"] ; then
-   sed "s/\${platform_url}/$platform_url/" stagingForJenkins/jenkins_home/config.xml >>  stagingForJenkins/jenkins_home/config.xml
-fi
+   sed "s/\${http.proxyHost}/$proxyHost/;s/\${http.proxyPort}/$proxyPort/;s/\${platform_url}/$platformurl/"g aggregatortech-jenkins-dv-repo/jenkins_home/config.xml >>  stagingForJenkins/jenkins_home/config.xml
 
 #create docker image
 docker build jenkins2/. -t jenkins/jenkins-ci
@@ -49,6 +48,6 @@ echo "volume recreated  successfully"
 chmod -R 777 stagingForJenkins
 docker cp  stagingForJenkins/jenkins_home/.  aggregatortech-jenkins-dv:/var/jenkins_home/.
 echo "volume refreshed  successfully"
-#rm -rf stagingForJenkins
+rm -rf stagingForJenkins
 sh startJenkins.sh $proxyHost $proxyPort
 
